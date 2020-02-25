742b9dc90618417779b2e2532ded15c1bd1ff5f8
Ayabulela Mahlathini <atrad75@gmail.com>
2020-02-26 00:47:05 +0200

    Object Identity

--- src/main/java/TestAdd.java
+++ src/main/java/TestAdd.java
@@ -1,8 +1,56 @@
 public class TestAdd {
 
+    private String name;
+    private int empId;
+    private int salary;
 
-    public int add(int x, int b) {
-        return x+b;
+    public TestAdd(int id, String name, int salary){
+        this.empId = id;
+        this.name = name;
+        this.salary = salary;
     }
 
+    public boolean equals(Object obj){
+        TestAdd employee = (TestAdd) obj;
+        boolean status = false;
+        if(this.name.equalsIgnoreCase(employee.name)
+                && this.empId == employee.empId
+                && this.salary == employee.salary){
+            status = true;
+        }
+        return status;
+    }
+
+    public static String getEmpNameWithHighestSalary(){
+
+        return "Ayabulela";
+    }
+
+    public static TestAdd getHighestPaidEmployee(){
+
+        return new TestAdd(1, "Ayabulela", 15000);
+    }
+
+    public int hashCode(){
+        return this.empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getEmpId() {
+        return empId;
+    }
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
--- src/test/TestAddTest.java
+++ src/test/TestAddTest.java
@@ -1,15 +1,42 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertSame;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class TestAddTest {
+
+    /*
+        @Test
+        public void mySimpleEqualsTest(){
+
+            String expectedName = "Ayabulela";
+            assertEquals(expectedName, TestAdd.getEmpNameWithHighestSalary());
+        }
+    */
+//Object Equality
     @Test
-    public void test(){
-        TestAdd test = new TestAdd();
-        int answer = test.add(10, 5);
-        assertEquals(15, answer);
+    public void myObjectEqualsTest(){
+
+        TestAdd expectedEmpObj = new TestAdd(1, "Ayabulela", 15000);
+        assertEquals(expectedEmpObj, TestAdd.getHighestPaidEmployee().getHighestPaidEmployee());
     }
 
 
+    //Object Identity
+    @Test
+    public void idtest1(){
+        TestAdd employee = new TestAdd(1,"Ayabulela", 15000);
+        Assert.assertSame(new TestAdd(1,"Ayabulela", 15000), employee);
+    } @Test
+    public void idtest2(){
+        TestAdd employee = new TestAdd(1,"Ayabulela", 15000);
+        Assert.assertNotSame(new TestAdd(1,"Ayabulela", 15000), employee);
+    }
 
 }
