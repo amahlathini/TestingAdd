6c122d2953e97700d098845b89d5e426f9242939
Ayabulela Mahlathini <atrad75@gmail.com>
2020-02-26 01:30:20 +0200

    TimeOUts

--- src/test/LargestTest.java
+++ src/test/LargestTest.java
@@ -1,7 +1,11 @@
 import org.junit.Test;
 
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofMinutes;
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
+//FailingTest
 public class LargestTest {
     @Test
     public void testEmpty() {
@@ -13,5 +17,28 @@ public class LargestTest {
             assertTrue(true);
         }
     }
+
+
+    //Timeouts
+
+    @Test
+   public void timeoutNotExceeded()
+    {
+        //The following assertion succeeds.
+        assertTimeout(ofMinutes(2), () -> {
+            // Perform task that takes less than 2 minutes.
+        });
+    }
+
+    @Test
+    public void timeoutExceeded()
+    {
+        // The following assertion fails with an error message
+        assertTimeout(ofMillis(10), () -> {
+            // Simulate task that takes more than 10 ms.
+            Thread.sleep(100);
+        });
+    }
+
 }
 
